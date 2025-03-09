1bc8f59de2bbcdeb064e4089c32bb07e235d75fd
Ruan <rugcosta1234@gmail.com>
2025-03-09 17:34:06 -0300

    Refactor ESLint configuration and enhance App component with random phrases feature

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,5 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-native/all',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    'react-native',
-  ],
-  rules: {
-    // Adicione ou ajuste regras conforme necessário
-  },
+  root: true,
+  extends: '@react-native',
+  "accessibility.underlineLinks": false
 };
--- App.js
+++ App.js
@@ -4,29 +4,50 @@ import {
   Text,
   StyleSheet,
   Image,
-  TextInput,
   TouchableOpacity,
   Alert,
 } from 'react-native';
 
 function App() {
   const [img, setImg] = useState(require('./src/biscoito.png'));
+  const [textoFrase, setTextoFrase] = useState('');
+
+  let frases = [
+    'A vida trará coisas boas se tiver paciência.',
+    'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.',
+    'Não compense na ira o que lhe falta na razão.',
+    'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+    'A maior de todas as torres começa no solo.',
+    'Não há que ser forte. Há que ser flexível.',
+    'Gente todo dia arruma os cabelos, por que não o coração?',
+    'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.',
+    'A juventude não é uma época da vida, é um estado de espírito.',
+    'Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.',
+  ];
+
+  function quebrarBiscoito() {
+    let numeroAleatorio = Math.floor(Math.random() * frases.length);
+    setTextoFrase(' " ' + frases[numeroAleatorio] + ' " ');
+    setImg(require('./src/biscoitoAberto.png'));
+  }
+
+  function reniciar() {
+    setImg(require('./src/biscoito.png'));
+    setTextoFrase('');
+  }
+
   return (
     <View style={styles.container}>
       <Image style={styles.img} source={img} />
-      <Text style={styles.text}>
-        " Esta é a minha primeira frase do biscoito! "
-      </Text>
-      <TouchableOpacity
-        onPress={() => Alert.alert('Test')}
-        style={styles.botao}>
+      <Text style={styles.text}>{textoFrase}</Text>
+      <TouchableOpacity onPress={quebrarBiscoito} style={styles.botao}>
         <View style={styles.btnArea}>
           <Text style={styles.btnText}>Quebrar biscoito</Text>
         </View>
       </TouchableOpacity>
       <TouchableOpacity
         style={[styles.botao, {marginTop: 10, borderColor: 'black'}]}
-        onPress={() => Alert.alert('Test')}>
+        onPress={reniciar}>
         <View style={styles.btnArea}>
           <Text style={[styles.btnText, {color: 'black'}]}>
             Reniciar Biscoito
